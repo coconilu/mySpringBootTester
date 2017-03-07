5a5e7fc3c4ede63f1f2c1470151aaca7adae9f5f
wangyaohui <827130441@qq.com>
2017-03-07 09:08:41 +0800

    使用SpringBoot默认的SLF4J记录日志

--- src/main/java/dev/wangyaohui/controller/HelloController.java
+++ src/main/java/dev/wangyaohui/controller/HelloController.java
@@ -1,15 +1,20 @@
 package dev.wangyaohui.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
-	
-	@RequestMapping
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @RequestMapping
 	String hello(){
-		return "hello booter!";
+        logger.info("access HelloController");
+        return "hello booter!";
 	}
 
 }
